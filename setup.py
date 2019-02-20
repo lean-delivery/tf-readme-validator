b28da385fa80290def5df80681a320d382cd2cae
Uladzimir Novichak <uladzimir_novichak@epam.com>
2019-02-20 12:34:30 +0300

    Add pyyaml to dependenies

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tf-readme-validator',
-      version='0.1',
+      version='0.2',
       description='Terraform README.md validator',
       url='https://github.com/lean-delivery/tf-readme-validator',
       author='Lean Delivery team',
@@ -9,6 +9,7 @@ setup(name='tf-readme-validator',
       license='Apache',
       scripts=['bin/tf_readme_validator.py'],
       install_requires=[
+        'pyyaml',
         'mistune',
       ],
       zip_safe=False)
